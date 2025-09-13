24d9a8faf4b611b36b69de12bbbbf3d6a1e359d4
RahulBalhara <rahulbalhara821@gmail.com>
2025-09-13 17:14:40 +0530

    initilization

--- cms/urls.py
+++ cms/urls.py
@@ -1,10 +1,13 @@
-from django.conf.urls import url
+from django.urls import re_path # <-- IMPORT UPDATED
 
 from cms import views
 
 app_name = "cms"
 
 urlpatterns = [
-    url('^$', views.home, name='home'),
-    url('^(?P<permalink>.+)$', views.home, name='home')
+    # This path is for your homepage
+    re_path(r'^$', views.home, name='home'), # <-- url changed to re_path
+
+    # This path is for any other page, identified by a permalink
+    re_path(r'^(?P<permalink>.+)$', views.home, name='page') # <-- url changed to re_path and name updated
 ]
